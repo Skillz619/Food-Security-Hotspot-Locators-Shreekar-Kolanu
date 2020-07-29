39869b3c7c3928b74c7a03399ee9186218ff8fa2
Shreekar Kolanu <43133388+Skillz619@users.noreply.github.com>
2020-07-30 00:06:03 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,255 @@
+let hospitals = [
+
+
+
+
+
+
+
+
+
+
+{
+    "name": "Uttar Pradesh-Jhansi",
+   "phoneNumber":"70% Covid Affected",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  25.447288,
+        "longitude": 78.504203,
+    },
+    "address":"Uttar Pradesh - Jhansi",
+    "addressLines":["4% Flood                                        (JHANSI)                "],
+    "pincode": "up", 
+    "zipCode":"515002",
+    "numBeds" : "95% Locust ",
+    "rating" : "145 Storages/Farms Affected",
+},
+{
+    "name": "Punjab-Mansa",
+   "phoneNumber":"60% Covid Affected",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  30.107003,
+        "longitude": 75.317355,
+    },
+    "address":"Punjab - Ludhiana",
+    "addressLines":["1% Flood                                                (LUDHIANA)        "],
+    "pincode": "punjab", 
+    "zipCode":"515002",
+    "numBeds" : "90% Locust ",
+    "rating" : "150 Storages/Farms Affected",
+},
+{
+    "name": "Rajasthan-Bikaner",
+   "phoneNumber":"50% Covid Affected",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  28.016539,
+        "longitude": 73.309446,
+    },
+    "address":"Rj - bikaner",
+    "addressLines":["2% Flood                                           (BIKANER)             "],
+    "pincode": "rajasthan", 
+    "zipCode":"515002",
+    "numBeds" : "100% Locust ",
+    "rating" : "250 Storages/Farms Affected",
+},
+{
+    "name": "Madhya Pradesh-Gwalior",
+   "phoneNumber":"80% Covid Affected",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  26.221027,
+        "longitude": 78.179765,
+    },
+    "address":"Rj - bikaner",
+    "addressLines":["3% Flood                                                   (GWALIOR)     "],
+    "pincode": "mp", 
+    "zipCode":"515002",
+    "numBeds" : "97% Locust ",
+    "rating" : "250 Storages/Farms Affected",
+},
+ 
+{
+    "name": "Assam-Guwahati",
+   "phoneNumber":"Covid Affected 65%",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  26.144214,
+        "longitude": 91.737205,
+    },
+    "address":"State Assam - Guwahati",
+    "addressLines":[" 80% Flood                                          (GUWAHATI)             "],
+    "pincode": "assam",
+    "zipCode":"781005",
+    "numBeds" : "Locust 0%",
+    "rating" : "150 Storages/Farms Affected",
+},
+
+{
+    "name": "Karnataka-Bangalore",
+   "phoneNumber":"55% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  12.932073,
+        "longitude": 77.719216,
+    },
+    "address":"State Karnataka - Bangalore",
+    "addressLines":["10% Flood                                   (BANGALORE)                     "],
+    "pincode": "karnataka",
+    "zipCode":"560087",
+    "numBeds" : "5% Locust ",
+    "rating" : "100 Storages/Farms Affected",
+},
+{
+    "name": "Karnataka-Raichur",
+   "phoneNumber":"85% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  16.216548,
+        "longitude": 77.354738,
+    },
+    "address":"State Karnataka - Raichur",
+    "addressLines":["20% Flood                                                (RAICHUR)        "],
+    "pincode": "karnataka",
+    "zipCode":"584101",
+    "numBeds" : "10% Locust ",
+    "rating" : "40 Storages/Farms Affected",
+},
+{
+    "name": "Karnataka-Ballari",
+   "phoneNumber":"75% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  15.117220,
+        "longitude": 76.907244,
+    },
+    "address":"State Karnataka - Ballari",
+    "addressLines":["5% Flood                                                  (BALLARI)      "],
+    "pincode": "karnataka",
+    "zipCode":"584101",
+    "numBeds" : "15% Locust ",
+    "rating" : "50 Storages/Farms Affected",
+},
+{
+    "name": "Andhra Pradesh-Anantapur",
+   "phoneNumber":"75% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  14.641501,
+        "longitude": 77.568806,
+    },
+    "address":"Andhra Pradesh - Anantapur",
+    "addressLines":["2% Flood                                                       (ANANTAPUR) "],
+    "pincode": "andhra",
+    "zipCode":"515002",
+    "numBeds" : "5% Locust ",
+    "rating" : "70 Storages/Farms Affected",
+},
+{
+    "name": "Andhra Pradesh-Kurnool",
+   "phoneNumber":"35% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  15.791005,
+        "longitude": 78.081614,
+    },
+    "address":"Andhra Pradesh - Kurnool",
+    "addressLines":["8% Flood                                                     (KURNOOL)   "],
+    "pincode": "andhra",
+    "zipCode":"515002",
+    "numBeds" : "12% Locust ",
+    "rating" : "60 Storages/Farms Affected",
+},
+{
+    "name": "Andhra Pradesh-Rajahmundry",
+   "phoneNumber":"55% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  17.011360,
+        "longitude": 81.825126,
+    },
+    "address":"Andhra Pradesh - Rajahmundry",
+    "addressLines":["18% Flood                                                     (RAJAHMUNDRY)   "],
+    "pincode": "andhra",
+    "zipCode":"515002",
+    "numBeds" : "4% Locust ",
+    "rating" : "20 Storages/Farms Affected",
+},
+{
+    "name": "Andhra Pradesh-Vizag",
+   "phoneNumber":"65% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  17.792830,
+        "longitude": 83.384537,
+    },
+    "address":"Andhra Pradesh - Vizag",
+    "addressLines":["48% Flood                                                      (VIZAG)  "],
+    "pincode": "andhra",
+    "zipCode":"515002",
+    "numBeds" : "4% Locust ",
+    "rating" : "100 Storages/Farms Affected",
+},
+{
+    "name": "Telangana-Nalagonda",
+   "phoneNumber":"75% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  17.053742,
+        "longitude": 79.265842,
+    },
+    "address":"Telangana - Nalagonda",
+    "addressLines":["18% Flood                                                     (NALAGONDA)  "],
+    "pincode": "Telangana", 
+    "zipCode":"515002",
+    "numBeds" : "8% Locust ",
+    "rating" : "63 Storages/Farms Affected",
+},
+{
+    "name": "Telangana-Warangal",
+   "phoneNumber":"35% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  17.956961,
+        "longitude": 79.615522,
+    },
+    "address":"Telangana - Warangal",
+    "addressLines":["13% Flood                                                    (WARANGAL)"],
+    "pincode": "Telangana" ,
+    "zipCode":"515002",
+    "numBeds" : "15% Locust ",
+    "rating" : "40 Storages/Farms Affected",
+},
+{
+    "name": "Telangana-Bhadrachalam",
+   "phoneNumber":"25% Covid Affected ",
+    "openTime" : "Food Security Affected Parameters",
+    "coordinates": {
+        "latitude":  17.669330,
+        "longitude": 80.893382,
+    },
+    "address":"Telangana - Bhadrachalam",
+    "addressLines":["8% Flood                                                         (BHADRACHALAM)"],
+    "pincode": "Telangana", 
+    "zipCode":"515002",
+    "numBeds" : "5% Locust ",
+    "rating" : "30 Storages/Farms Affected",
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
